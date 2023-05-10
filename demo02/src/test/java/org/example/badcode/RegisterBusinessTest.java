2c7d1ff279b88060b7559b2e2cb482053151d420
up1 <somkiat.p@gmail.com>
2023-05-10 15:56:05 +0700

    Add first case with throw exception

---
+++ demo02/src/test/java/org/example/badcode/RegisterBusinessTest.java
@@ -0,0 +1,36 @@
+package org.example.badcode;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RegisterBusinessTest {
+
+    @Test
+    @DisplayName("Register without firstname")
+    public void case01_exception_manual() {
+        // Arrange
+        RegisterBusiness registerBusiness = new RegisterBusiness();
+        // Act
+        try {
+            registerBusiness.register(null, new Speaker());
+            fail();
+        }catch (Exception e) {
+            assertEquals("First name is required.", e.getMessage());
+        }
+    }
+
+    @Test
+    @DisplayName("Register without firstname with junit 5")
+    public void case01_exception_junit5() {
+        // Arrange
+        RegisterBusiness registerBusiness = new RegisterBusiness();
+        // Act
+
+        Exception e =  assertThrows(ArgumentNullException.class, () -> {
+            registerBusiness.register(null, new Speaker());
+        });
+        assertEquals("First name is required.", e.getMessage());
+    }
+}

232e974434233c74932e55e148a34f75c94862f6
up1 <somkiat.p@gmail.com>
2023-05-11 16:37:05 +0700

    Refactor code

--- day02/src/main/java/com/example/day02/UserGateway.java
+++ day02/src/main/java/com/example/day02/UserGateway.java
@@ -1,6 +1,7 @@
 package com.example.day02;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -10,8 +11,11 @@ public class UserGateway {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Value("${my_url}")
+    private String myUrl;
+
     public UserResponse getUnitById(int userId) {
-        String targetUrl = "https://jsonplaceholder.typicode.com/users/" + userId;
+        String targetUrl = myUrl + "/users/" + userId;
         return restTemplate.getForObject(targetUrl, UserResponse.class);
     }
 

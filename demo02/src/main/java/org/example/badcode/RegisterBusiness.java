83c2046c453d2b84417c691c6390c356d1e728db
up1 <somkiat.p@gmail.com>
2023-05-11 11:12:49 +0700

    Parameterized test with calculate fee from exp

--- demo02/src/main/java/org/example/badcode/RegisterBusiness.java
+++ demo02/src/main/java/org/example/badcode/RegisterBusiness.java
@@ -36,7 +36,7 @@ public class RegisterBusiness {
         }
     }
 
-    int getFee(int experienceYear) {
+    public int getFee(int experienceYear) {
         int fee = 0;
         if (experienceYear <= 1) {
             fee = 500;
---
+++ demo02/src/test/java/org/example/badcode/RegisterFeeTest.java
@@ -0,0 +1,25 @@
+package org.example.badcode;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class RegisterFeeTest {
+
+    @ParameterizedTest
+    @CsvSource({
+            "1, 500",
+            "3, 250",
+            "5, 100",
+            "9, 50",
+            "15, 0"
+    })
+    public void getFee(int experienceYear, int expectedFee) {
+        RegisterBusiness registerBusiness = new RegisterBusiness();
+        int actualFee = registerBusiness.getFee(experienceYear);
+        assertEquals(expectedFee, actualFee);
+    }
+}

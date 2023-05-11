29dc0550cc18fdeee22ce8d30200abcaddde8091
up1 <somkiat.p@gmail.com>
2023-05-11 09:36:36 +0700

    Demo with spy

--- demo02/src/test/java/org/example/badcode/RegisterBusinessSuccessTest.java
+++ demo02/src/test/java/org/example/badcode/RegisterBusinessSuccessTest.java
@@ -17,14 +17,45 @@ class StubSpeakerRepository implements SpeakerRepository {
     }
 }
 
+
+class SpySpeakerRepository implements SpeakerRepository {
+    private int called;
+    public boolean verify(int expectedCall) {
+        return this.called == expectedCall;
+    }
+    @Override
+    public Integer saveSpeaker(Speaker speaker) {
+        this.called++;
+        return null;
+    }
+}
+
 public class RegisterBusinessSuccessTest {
+
+    @Test
+    @DisplayName("Register success with id = 5")
+    public void register_success_with_spy(){
+        // Arrange
+        SpySpeakerRepository spy = new SpySpeakerRepository();
+
+        Speaker speaker = new Speaker();
+        speaker.setFirstName("Somkiat");
+        speaker.setLastName("Pui");
+        speaker.setEmail("xxxx@gmail.com");
+
+        RegisterBusiness registerBusiness = new RegisterBusiness();
+        // Act
+        registerBusiness.register(spy, speaker);
+        // Assert
+        assertTrue(spy.verify(1));
+    }
 
     @Test
     @DisplayName("Register success with id = 5")
     public void register_success(){
         // Arrange
         StubSpeakerRepository stub = new StubSpeakerRepository();
-        stub.initData(11);
+        stub.initData(1000);
 
         Speaker speaker = new Speaker();
         speaker.setFirstName("Somkiat");

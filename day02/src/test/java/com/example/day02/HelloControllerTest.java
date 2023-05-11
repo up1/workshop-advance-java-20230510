92ba6052d1f086eb0a4649489901db128506661a
up1 <somkiat.p@gmail.com>
2023-05-11 14:05:56 +0700

    Get data with entity

--- day02/src/test/java/com/example/day02/HelloControllerTest.java
+++ day02/src/test/java/com/example/day02/HelloControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.boot.test.context.SpringBootTest.*;
@@ -24,6 +25,18 @@ public class HelloControllerTest {
         // Assert
         assertEquals(1, result.getId());
         assertEquals("Hello Spring Boot", result.getMessage());
+    }
+
+    @Test
+    @DisplayName("Success with api /hello/say + entity")
+    public void success_case02() {
+        // Act
+        ResponseEntity<MessageResponse> result
+                = restTemplate.getForEntity("/hello/say", MessageResponse.class);
+        // Assert
+        assertEquals(200, result.getStatusCode().value());
+        assertEquals(1, result.getBody().getId());
+        assertEquals("Hello Spring Boot", result.getBody().getMessage());
     }
 
 }
